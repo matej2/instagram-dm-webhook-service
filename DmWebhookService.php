8cf1b852ae6ed73eeb8d6b18164f0d2ee39756b5
matej2 <11059438+matej2@users.noreply.github.com>
2019-06-13 21:10:06 +0200

    Added array config functionality

--- DmWebhookService.php
+++ DmWebhookService.php
@@ -6,24 +6,36 @@ class InstagramChatBot
   public function __construct () {
     $this->config = json_decode(file_get_contents('./config.json'));
   }
-  public function send() {
-      $url = $this->config->url;
-      $data = array('key1' => 'value1', 'key2' => 'value2');
+  public function send($config) {
+    $url = $config->url;
+    $data = array('key1' => 'value1', 'key2' => 'value2');
 
-      // use key 'http' even if you send the request to https://...
-      $options = array(
-        'http' => array(
-          'header'  => "Content-type: application/json",
-          'method'  => $this->config->method,
-          'content' => json_encode($data)
-        )
-      );
-      $context  = stream_context_create($options);
-      $result = file_get_contents($url, false, $context);
-      var_dump($http_response_header);
-      if ($result === FALSE) { 
-        $result.="False";
-      }
-      return $result.$http_response_header;
+    var_dump($config);
+
+    // use key 'http' even if you send the request to https://...
+    $options = array(
+      'http' => array(
+        'header'  => "Content-type: application/json",
+        'method'  => $config->method,
+        'content' => json_encode($data)
+      )
+    );
+    $context  = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+    var_dump($http_response_header);
+    if ($result === FALSE) { 
+      $result.="False";
     }
+    //return $result.$http_response_header;
+    return true;
+  }
+
+  public function getSingleConfig($index) {
+    if(isset($index) && gettype($index) == "integer" && $index > -1 && isset($this->config[$index])) {
+      return $this->config[$index];
+    }
+    else {
+      return false;
+    }
+  }
 }
